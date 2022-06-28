78419a6a24a2ec68c943f31def0b4b3c8be4e4e0
Aditya Aggarwal <85567110+WhackyBoyg0d@users.noreply.github.com>
2022-06-28 21:05:36 +0530

    Sounds to Buttons

--- game.js
+++ game.js
@@ -1,8 +1,27 @@
 var buttonColours = ["red", "blue", "green", "yellow"]
 var gamePattern = [];
+var userClickedPattern = [];
 
-function nextSequence (buttonColours, gamePattern) {
+function nextSequence () {
     var randomNumber = Math.floor(Math.random() * 4 );
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+    $("#randomChosenColour").fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
 }
+
+function playSound(name) {
+    var audio = new Audio("sounds/"+name+".mp3");
+    audio.play();
+}
+
+$(".btn").click(function() {
+    var userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+    playSound(this.id);
+})
+
+
+
+
