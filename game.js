388b063b4b9a509ea0098f96b70006ad50af453d
Aditya Aggarwal <85567110+WhackyBoyg0d@users.noreply.github.com>
2022-06-29 01:28:09 +0530

    Animation to Buttons

--- game.js
+++ game.js
@@ -15,12 +15,22 @@ function playSound(name) {
     audio.play();
 }
 
+function animatePress(currentColour) {
+    $('#' + currentColour).addClass("pressed").dequeue().delay(100).queue(function () {
+        $(this).removeClass("pressed");
+});
+}
+
 $(".btn").click(function() {
+    animatePress(this.id);
     var userChosenColour = this.id;
     userClickedPattern.push(userChosenColour);
     console.log(userClickedPattern);
     playSound(this.id);
 })
+
+
+
 
 
 
